4bae6c0ccb51ed5759acfd7723217f89dd59420a
rohit-batra-1307 <135214621+rohit-batra-1307@users.noreply.github.com>
2024-06-06 18:56:46 +0530

    neo4jSession.js

--- server/neo4jSession.js
+++ server/neo4jSession.js
@@ -1,8 +1,8 @@
 import neo4j from 'neo4j-driver';
 
-const NEO4J_URI = process.env.NEO4J_URI || 'neo4j+s://45bef46c.databases.neo4j.io';
+const NEO4J_URI = process.env.NEO4J_URI || 'neo4j+s://10a90882.databases.neo4j.io';
 const NEO4J_USER = process.env.NEO4J_USER || 'neo4j';
-const NEO4J_PASSWORD = process.env.NEO4J_PASSWORD || 'UQVF754VKMMzf6f2s6n28frgt9AHi7ddMmSzLWuebNE';
+const NEO4J_PASSWORD = process.env.NEO4J_PASSWORD || 'Ay56y4OUQMeEbERPmqbgoHOfRi74lg5CEWXgtu5SZuk';
 
 const driver = neo4j.driver(NEO4J_URI, neo4j.auth.basic(NEO4J_USER, NEO4J_PASSWORD));
 
